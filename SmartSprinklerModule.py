d0235875dd739aaf6ae188f6cff3428b1cce6b7a
Cvitanic, Toni <tcvitanic3@gatech.edu>
2016-03-08 11:02:48 -0500

    added rotate motor and center target functions to the main module. Also adjusted main module to follow good python guidelines (as suggested by PyCharm

--- SmartSprinklerModule.py
+++ SmartSprinklerModule.py
@@ -1,15 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+import math
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import os
 
 # Capture an Image Directly from the Webcam:
+
+
 def capture_image(ramp_frames=10, average_frames=10, file_name='null'):
     # Function to capture an image via webcam. Requires cv2, numpy, and os.
     # Optional Input Arguments:
     #       - ramp_frames is the number of frames to wait for the camera to calibrate
     #       - average_frames is the number of frames to average (effectively a low pass filter)
-    #       - file_name is an argument that saves the image to a local file. If not specified then no image will be saved.
+    #       - file_name is an argument that saves the image to a local file. If not specified then no image will be
+    #         saved.
     # Output Arguments:
     #       - camera_capture is the averaged  image variable
     
@@ -30,29 +36,162 @@ def capture_image(ramp_frames=10, average_frames=10, file_name='null'):
     camera_capture = get_image()
     for i in xrange(average_frames-1):
         camera_capture = camera_capture.astype(float) + get_image()
-    camera_capture = np.divide(camera_capture,average_frames)
+    camera_capture = np.divide(camera_capture, average_frames)
     camera_capture = camera_capture.astype('uint8')     # Convert to integer
     
     if file_name != 'null':
         print("Saving Image File ...")
         path = os.curdir + '/'                  # Save in the current directory
         file_name = file_name + '.png'          # Default .png
-        cv2.imwrite(path+file_name,camera_capture)
+        cv2.imwrite(path+file_name, camera_capture)
 
     return camera_capture
 
 # Determine the Centroid of an Image
-def find_centroid(img,level=240):
-    ret,thresh = cv2.threshold(img,level,255,0)
+
+
+def find_centroid(img, level=240):
+    ret, thresh = cv2.threshold(img, level, 255, 0)
     if thresh.max() < 255:
         FLAME_DETECTED = 0
         cx = []
         cy = []
     else:
         FLAME_DETECTED = 1
-        contours,hierarchy = cv2.findContours(thresh,1,2)
+        contours, hierarchy = cv2.findContours(thresh, 1, 2)
         M = cv2.moments(contours[len(contours)-1])
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
-    return(FLAME_DETECTED,cx,cy)
+    return FLAME_DETECTED, cx, cy
 
+
+def rotate_motor(direction, angle):
+    # This function rotates the desired motor to the specified angle.
+    # direction can either be pan or tilt
+    # angle is an angle rotation in radians
+
+    if direction is not 'pan' and direction is not 'tilt':
+        print 'Please choose either pan or tilt for the direction'
+        exit()
+
+    if angle < -math.pi / 2:
+        print 'Reached lowest angle for ' + direction
+        angle = -math.pi / 2
+    elif angle > math.pi / 2:
+        print 'Reached maximum angle for ' + direction
+        angle = math.pi / 2
+
+    GPIO.setmode(GPIO.BCM)
+    if direction == 'pan':
+        GPIO.setup(17, GPIO.OUT)
+        channel = 17
+    else:
+        GPIO.setup(18, GPIO.OUT)
+        channel = 18
+
+    if angle <= 0:
+        duty_cycle = (math.pi / 2 - abs(angle)) * 5.5 / (math.pi / 2) + 2
+    else:
+        duty_cycle = angle * (5.5 / (math.pi / 2)) + 7.5
+    print 'the duty cycle is ' + str(duty_cycle) + '%'
+
+    p = GPIO.PWM(channel, 50)
+    p.start(duty_cycle)
+    time.sleep(1)
+    p.stop()
+
+    return angle
+
+
+def center_target(pan_angle, tilt_angle, initial_rotation=.9):
+    # This function commands the motors to adjust the camera until the centroid of the fire is brought to the center of
+    # the image
+    # pan_angle and tilt_angle indicate the current pan and tilt angles of the camera
+    # initial_rotation indicates the angle by which the camera will rotate to calibrate its gains
+
+    # The function returns 1 if successful, and 0 if unsuccessful
+
+    # Define the image size
+    x_max = 480
+    y_max = 640
+
+    # Capture an initial image
+    img = capture_image()
+    flame, cx, cy = find_centroid(img)
+
+    # Calculate how far the x and y coordinates of the centroid are from the center of the image
+    x_offset = float(x_max) / 2.0 - cx
+    y_offset = float(y_max) / 2.0 - cy
+
+    # Define a tolerance of how close you want cx and cy to be to the center of the image (in pixels)
+    tolerance = 3
+
+    # Do an initial rotation to calibrate gains
+    if abs(x_offset) > tolerance:
+        if x_offset < 0:
+            pan_angle = rotate_motor('pan', pan_angle + initial_rotation)
+        else:
+            pan_angle = rotate_motor('pan', pan_angle - initial_rotation)
+    if abs(y_offset) > tolerance:
+        if y_offset < 0:
+            tilt_angle = rotate_motor('tilt', tilt_angle + initial_rotation)
+        else:
+            tilt_angle = rotate_motor('tilt', tilt_angle - initial_rotation)
+
+    img = capture_image()
+    flame, cx, cy = find_centroid(img)
+    if not flame:
+        print 'Fire no longer detected after small angle change. Either the camera is too close to the fire, or the' \
+              ' fire has gone out'
+        exit()
+
+    new_x_offset = float(x_max) / 2.0 - cx
+    new_y_offset = float(y_max) / 2.0 - cy
+
+    # Calculate gains. Gains refer to how far the camera moved versus how many pixels the centroid coordinates changed
+    # by.
+    x_change = new_x_offset - x_offset
+    y_change = new_y_offset - y_offset
+    if x_change == 0 or y_change == 0:
+        print('The initial turret rotation is too small. Please choose a larger initial rotation.')
+        exit()
+
+    x_gain = initial_rotation / abs(x_change)
+    y_gain = initial_rotation / abs(y_change)
+
+    # Repeat this process, updating the gains every iteration, until convergence
+    while abs(x_offset) > tolerance or abs(y_offset) > tolerance:
+        if abs(x_offset) > tolerance:
+            if x_offset < 0:
+                rotate_motor('pan', pan_angle + abs(x_offset) * x_gain)
+            else:
+                rotate_motor('pan', pan_angle - x_offset * x_gain)
+        if abs(y_offset) > tolerance:
+            if y_offset < 0:
+                rotate_motor('tilt', tilt_angle + abs(y_offset) * y_gain)
+            else:
+                rotate_motor('tilt', tilt_angle - y_offset * y_gain)
+
+        img = capture_image()
+        flame, cx, cy = find_centroid(img)
+        if not flame:
+            print 'Flame lost'
+            return 0
+
+        new_x_offset = float(x_max) / 2.0 - cx
+        new_y_offset = float(y_max) / 2.0 - cy
+
+        x_change = new_x_offset - x_offset
+        y_change = new_y_offset - y_offset
+
+        if x_change == 0 or y_change == 0:
+            print 'The gain is too small. Consider adding higher proportional or integral gains'
+            exit()
+
+        x_gain = abs(x_offset * x_gain) / abs(x_change)
+        y_gain = abs(y_offset * y_gain) / abs(y_change)
+
+        x_offset = new_x_offset
+        y_offset = new_y_offset
+
+    return 1
