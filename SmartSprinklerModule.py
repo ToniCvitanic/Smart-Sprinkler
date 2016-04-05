468760fdf45e973f436eb70a511d49a232476187
tonicvitanic <tonicvitanic@gmail.com>
2016-04-05 14:40:32 +0000

    new method for getting gains based on near, mid, or long range target. Kept the old way just in case.

--- SmartSprinklerModule.py
+++ SmartSprinklerModule.py
@@ -128,7 +128,7 @@ def rotate_motor(direction, angle):
     return angle
 
 
-def center_target(pan_angle, tilt_angle, cx, cy, initial_rotation=2*math.pi/180):
+def center_target(pan_angle, tilt_angle, cx, cy, initial_rotation=3*math.pi/180, initial_gain=.1):
     # This function commands the motors to adjust the camera until the centroid of the fire is brought to the center of
     # the image
     # pan_angle and tilt_angle indicate the current pan and tilt angles of the camera
@@ -140,13 +140,16 @@ def center_target(pan_angle, tilt_angle, cx, cy, initial_rotation=2*math.pi/180)
     x_max = 640
     y_max = 480
 
+    mid_gain = .5
+    far_gain = 1
+
     # Capture an initial image
     #img = capture_image()
     #flame, cx, cy = find_centroid(img)
 
     # Calculate how far the x and y coordinates of the centroid are from the center of the image
-    x_offset = float(x_max) / 2.0 - cx
-    y_offset = float(y_max) / 2.0 - cy
+    x_offset = cx - float(x_max) / 2.0
+    y_offset = cy - float(y_max) / 2.0
 
     print 'the x offset is ' + str(x_offset)
     print 'the y offset is ' + str(y_offset)
@@ -154,75 +157,96 @@ def center_target(pan_angle, tilt_angle, cx, cy, initial_rotation=2*math.pi/180)
     # Define a tolerance of how close you want cx and cy to be to the center of the image (in pixels)
     tolerance = 3
 
-    # Do an initial rotation to calibrate gains
+    # Start targeting with initial angle change
     if abs(x_offset) > tolerance:
         if x_offset < 0:
-            pan_angle = rotate_motor('pan', pan_angle - initial_rotation)
-        else:
             pan_angle = rotate_motor('pan', pan_angle + initial_rotation)
+        else:
+            pan_angle = rotate_motor('pan', pan_angle - initial_rotation)
     if abs(y_offset) > tolerance:
         if y_offset < 0:
-            tilt_angle = rotate_motor('tilt', tilt_angle + initial_rotation)
-        else:
             tilt_angle = rotate_motor('tilt', tilt_angle - initial_rotation)
+        else:
+            tilt_angle = rotate_motor('tilt', tilt_angle + initial_rotation)
     i = 1
-    img = capture_image(10,10,'centerimage' + str(i))
+    img = capture_image(3,3,'centerimage' + str(i))
     flame, cx, cy = find_centroid(img)
     if not flame:
-        print 'Fire no longer detected after small angle change. Either the camera is too close to the fire, or the' \
-              ' fire has gone out'
+        print 'Lost flame after first rotation'
         exit()
 
-    new_x_offset = float(x_max) / 2.0 - cx
-    new_y_offset = float(y_max) / 2.0 - cy
+    new_x_offset = cx - float(x_max) / 2.0
+    new_y_offset = cy - float(y_max) / 2.0
 
-    # Calculate gains. Gains refer to how far the camera moved versus how many pixels the centroid coordinates changed
-    # by.
-    x_change = new_x_offset - x_offset
-    y_change = new_y_offset - y_offset
-    if x_change == 0 or y_change == 0:
-        print('The initial turret rotation is too small. Please choose a larger initial rotation.')
-        exit()
-
-    x_gain = .1 * initial_rotation / abs(x_change)
-    y_gain = .1 * initial_rotation / abs(y_change)
+    x_change = [new_x_offset - x_offset]
+    y_change = [new_y_offset - y_offset]
 
     x_offset = new_x_offset
     y_offset = new_y_offset
 
     # Repeat this process, updating the gains every iteration, until convergence
     while abs(x_offset) > tolerance or abs(y_offset) > tolerance:
+        while i <= 3:
+            print 'the x offset is ' + str(x_offset)
+            print 'the y offset is ' + str(y_offset)
+            if abs(x_offset) > tolerance:
+                if x_offset < 0:
+                    rotate_motor('pan', pan_angle + abs(x_offset) * initial_gain)
+                else:
+                    rotate_motor('pan', pan_angle - x_offset * initial_gain)
+            if abs(y_offset) > tolerance:
+                if y_offset < 0:
+                    rotate_motor('tilt', tilt_angle - abs(y_offset) * initial_gain)
+                else:
+                    rotate_motor('tilt', tilt_angle + y_offset * initial_gain)
+            i = i + 1
+            img = capture_image(3,3,'centerimage' + str(i))
+            flame, cx, cy = find_centroid(img)
+            if not flame:
+                print 'Flame lost'
+                return 0
+
+            new_x_offset = cx - float(x_max) / 2.0
+            new_y_offset = cy - float(y_max) / 2.0
+
+            x_change.append(new_x_offset - x_offset)
+            y_change.append(new_y_offset - y_offset)
+
+            x_offset = new_x_offset
+            y_offset = new_y_offset
+
+        ave_x_change = (abs(x_change[1]) + abs(x_change[2]) + abs(x_change[3]))/3
+        ave_y_change = (abs(y_change[1]) + abs(y_change[2]) + abs(y_change[3]))/3
+
+        if x_offset > 100 and y_offset > 100 and i == 4:
+            if ave_x_change < 10 or ave_y_change < 10:
+                print 'using far gain'
+                gain = far_gain
+            elif ave_ave_x_change < 30 or ave_y_change < 30:
+                print 'using mid gain'
+                gain = mid_gain
+                
         print 'the x offset is ' + str(x_offset)
         print 'the y offset is ' + str(y_offset)
         if abs(x_offset) > tolerance:
             if x_offset < 0:
-                rotate_motor('pan', pan_angle - abs(x_offset) * x_gain)
+                rotate_motor('pan', pan_angle + abs(x_offset) * gain)
             else:
-                rotate_motor('pan', pan_angle + x_offset * x_gain)
+                rotate_motor('pan', pan_angle - x_offset * gain)
         if abs(y_offset) > tolerance:
             if y_offset < 0:
-                rotate_motor('tilt', tilt_angle + abs(y_offset) * y_gain)
+                rotate_motor('tilt', tilt_angle - abs(y_offset) * gain)
             else:
-                rotate_motor('tilt', tilt_angle - y_offset * y_gain)
+                rotate_motor('tilt', tilt_angle + y_offset * gain)
         i = i + 1
-        img = capture_image(10,10,'centerimage' + str(i))
+        img = capture_image(3,3,'centerimage' + str(i))
         flame, cx, cy = find_centroid(img)
         if not flame:
             print 'Flame lost'
             return 0
 
-        new_x_offset = float(x_max) / 2.0 - cx
-        new_y_offset = float(y_max) / 2.0 - cy
-
-        x_change = new_x_offset - x_offset
-        y_change = new_y_offset - y_offset
-
-        if x_change == 0 or y_change == 0:
-            print 'The gain is too small. Consider adding higher proportional or integral gains'
-            exit()
-
-        x_gain = .1 * abs(x_offset * x_gain) / abs(x_change)
-        y_gain = .1 * abs(y_offset * y_gain) / abs(y_change)
+        new_x_offset = cx - float(x_max) / 2.0
+        new_y_offset = cy - float(y_max) / 2.0
 
         x_offset = new_x_offset
         y_offset = new_y_offset
--- firetracker.py
+++ firetracker.py
@@ -7,24 +7,25 @@ tilt_angle = SSM.rotate_motor('tilt', -1.1)
 
 i = 1
 while 1:
-    image = SSM.capture_image(10,10,'image' + str(i))
+    image = SSM.capture_image(3,3,'image' + str(i))
     flame_present, x_centroid, y_centroid, edge_crossing = SSM.find_centroid(image)
     if flame_present:
         print 'Flame detected!'
-        if edge_crossing == 1:
-            ssm.rotate_motor('pan', 10 * math.pi / 180)
-            target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
-        elif edge_crossing == 2:
-            ssm.rotate_motor('pan', -10 * math.pi / 180)
-            target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
-        elif edge_crossing == 3:
-            ssm.rotate_motor('tilt', -10 * math.pi / 180)
-            target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
-        elif edge_crossing == 4:
-            ssm.rotate_motor('tilt', 10 * math.pi / 180)
-            target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
-        else:
-            target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        #if edge_crossing == 1:
+            #ssm.rotate_motor('pan', 10 * math.pi / 180)
+            #target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        #elif edge_crossing == 2:
+            #ssm.rotate_motor('pan', -10 * math.pi / 180)
+            #target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        #elif edge_crossing == 3:
+            #ssm.rotate_motor('tilt', -10 * math.pi / 180)
+            #target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        #elif edge_crossing == 4:
+            #ssm.rotate_motor('tilt', 10 * math.pi / 180)
+            #target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
+        #else:
+            #target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
         if target_centered:
             print 'target is centered'
         else:
@@ -36,5 +37,5 @@ while 1:
         if pan_angle > math.pi / 2 - .09:
             pan_angle = SSM.rotate_motor('pan', -1.3)
         else:
-            pan_angle = SSM.rotate_motor('pan', pan_angle + 20 * math.pi / 180)
+            pan_angle = SSM.rotate_motor('pan', pan_angle + 10 * math.pi / 180)
     i = i+1
