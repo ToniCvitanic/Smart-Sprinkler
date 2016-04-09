d3d3cb182b81e8c3aa371e051b27d24565015158
Cvitanic, Toni <tcvitanic3@gatech.edu>
2016-04-09 15:18:16 -0400

    added comments on how the code works, and got rid of saving images

--- firescan.py
+++ firescan.py
@@ -1,7 +1,10 @@
 import SmartSprinklerModule as SSM
 import math
 
-# Initialize the position of the sprinkler to be at -90 degrees in pan, and -45 degrees in tilt
+# This script is the main script to run the Smart Sprinkler. It continuously scans for, targets, and squirts water at
+# fires as long as the device is turned on
+
+# Initialize the position of the sprinkler position
 pan_angle = SSM.rotate_motor('pan', -1.3)
 tilt_angle = SSM.rotate_motor('tilt', -1.5)
 left_check = 0
@@ -9,17 +12,25 @@ right_check = 0
 bottom_check = 0
 top_check = 0
 
+# Define the height of the image (used later)
 y_max = 480
 
+# Define the different tilt angles to run through. They are repeated because of the way the code is written in
+# incrementing them.
 tilt_ind = 0
 tilt_scan_angles = [1.5, 1.5, 1, 1, .5, .5]
 
 i = 1
 while 1:
-    image = SSM.capture_image(1,1,'image' + str(i))
+    # Capture an image
+
+    #image = SSM.capture_image(1,1,'image' + str(i))
+    image = SSM.capture_image()
+    # Check for a flame, and find its centroid in the image
     flame_present, x_centroid, y_centroid, edge_crossing = SSM.find_centroid(image)
     if flame_present:
         print 'Flame detected!'
+        # Check if the flame centroid touches any edges of the image
         for k in range(0, len(edge_crossing)):
             if edge_crossing[k] == 1:
                 left_check = 1
@@ -30,6 +41,7 @@ while 1:
             elif edge_crossing[k] == 4:
                 bottom_check = 1
         y_offset = y_centroid - float(y_max) / 2.0
+        # Move to get centroid of the flame away from the edges of the image
         if left_check == 1:
             pan_angle = SSM.rotate_motor('pan', pan_angle + (-3 if y_offset < 0 else 3) * math.pi / 180)
         elif right_check == 1:
@@ -41,17 +53,20 @@ while 1:
         if left_check == 1 or right_check == 1 or bottom_check == 1 or top_check == 1:
             flame_present, x_centroid, y_centroid, edge_crossing = SSM.find_centroid(image)
 
+        # Bring the centroid of the fire to the center of the image
         target_centered = SSM.center_target(pan_angle, tilt_angle, x_centroid, y_centroid)
 
         if target_centered:
             j = 1
+            # Spray water until the flame is no longer present
             while flame_present:
                 print 'Spraying water'
                 SSM.spray_water()
-                image = SSM.capture_image(1,1,'flameimage' + str(j))
+                image = SSM.capture_image()
+                #image = SSM.capture_image(1,1,'flameimage' + str(j))
                 flame_present, x_centroid, y_centroid, edge_crossing = SSM.find_centroid(image)
-                 #If initial spray did not extinguish the fire, spray upwards in increments until it has been
-                 #extinguished
+                # If initial spray did not extinguish the fire, spray at slightly different tilts until it is
+                # extinguished
                 if flame_present:
                     y_offset = y_centroid - float(y_max) / 2.0
                     if y_offset > 0:
